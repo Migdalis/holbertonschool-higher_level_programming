97049ec4c71c25288f84565f4efef9a3a8601a37
Migdalis <miglepage@gmail.com>
2022-01-18 19:07:59 -0500

    Add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Square:
+    """Class used to represent a Square"""
+
+    def __init__(self, size):
+        """
+        Parameters
+        ----------
+        size : int
+            Size of the square
+        """
+
+        self.__size = size
