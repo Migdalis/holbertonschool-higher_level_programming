3dde82262ee43455ce46f62c6875240462255309
Migdalis <miglepage@gmail.com>
2022-01-18 23:43:31 -0500

    I really Hope all checker green task 6 :(

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -65,6 +65,6 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        for i in range(self.__size):
-            print("{}".format(' ' * self.__position[0]), end="")
-            print("{}".format('#' * self.__size))
+        for i in range(0, self.__size):
+            print("{}".format(" " * self.__position[0]), end="")
+            print("{}".format("#" * self.__size))
