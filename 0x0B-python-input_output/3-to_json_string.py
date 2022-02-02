104698ad9bb56e6eb60bfda5b3f17da50da7e139
Migdalis <miglepage@gmail.com>
2022-02-01 20:36:02 -0500

    Add task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+"""Define a function that returns the JSON representation"""
+
+
+def to_json_string(my_obj):
+    """
+        Function that returns the JSON representation of an object (string)
+        Parameters
+        ----------
+        my_obj : object to encode
+        Returns
+        -------
+            The JSON representation
+    """
+    return json.dumps(my_obj)
