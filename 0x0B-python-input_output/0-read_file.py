ad09248fce63caf933dde8cc4ffc8278a856b459
Migdalis <miglepage@gmail.com>
2022-02-01 19:45:49 -0500

    Add task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a function that reads a text file"""
+
+
+def read_file(filename=""):
+    """Function that reads a text file in UTF8 and prints it to stdout"""
+
+    with open(filename, encoding="utf-8") as m_file:
+        print(m_file.read())
