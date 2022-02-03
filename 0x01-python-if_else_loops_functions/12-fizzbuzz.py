03f574e2f0ba0d25f144207099621eea56eebb88
Migdalis <miglepage@gmail.com>
2022-02-03 17:48:38 -0500

    Add task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for i in range(1, 101):
         mult_t = i % 3
         mult_f = i % 5
-        
+
         if ((mult_t == 0) and (mult_f == 0)):
             print("FizzBuzz ", end="")
         elif (mult_t == 0):
