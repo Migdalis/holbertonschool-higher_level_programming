6f5eb0553ac7f2794c50b0b87f1cae2d9f71fddc
Migdalis <miglepage@gmail.com>
2022-04-04 22:56:12 -0500

    Fix task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  const x = parseInt(process.argv[2]);
+  const x = process.argv[2];
   for (let i = 0; i < x; i++) {
     console.log('x'.repeat(x));
   }
