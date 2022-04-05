fb2952df2dca73e1d68595434c478b9f2bdc3bfe
Migdalis <miglepage@gmail.com>
2022-04-04 21:50:21 -0500

    Fix semistandard task 6 and 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@
 if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  let x = parseInt(process.argv[2]);
+  const x = parseInt(process.argv[2]);
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
