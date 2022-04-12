629bc587bff8b088bede7c2fb2e7cbc9f3412870
Migdalis <miglepage@gmail.com>
2022-04-12 17:51:01 -0500

    Fix task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,14 @@
 
 const request = require('request');
 const url = process.argv[2];
+const id = 18;
 
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
     const listFilms = JSON.parse(body).results;
-    const character = 'https://swapi-api.hbtn.io/api/people/18/';
+    const character = 'https://swapi-api.hbtn.io/api/people/' + id + '/';
     let count = 0;
     for (const element of listFilms) {
       if (element.characters.includes(character)) {
