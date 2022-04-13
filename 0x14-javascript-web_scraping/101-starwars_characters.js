0a0d4e6d15032463b48cfe53476ef48a9534a535
Migdalis <miglepage@gmail.com>
2022-04-12 23:46:45 -0500

    Try something

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,27 +1,21 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
 
-request('https://swapi-api.hbtn.io/api/films/', (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    const listFilms = JSON.parse(body).results;
-    for (const film of listFilms) {
-      if (film.url.includes(process.argv[2])) {
-        console.log(film.url);
-        const listPeople = film.characters;
-        for (const people of listPeople) {
-          request(people, (error, response, body) => {
-            if (error) {
-              console.log(error);
-            } else {
-              console.log(JSON.parse(body).name);
-            }
-          });
+    const listPeople = JSON.parse(body).characters;
+    for (const people of listPeople) {
+      request(people, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
         }
-        break;
-      }
+      });
     }
   }
 });
