5cf0128af6d96b1674d88d7d3de2a291492edab7
Migdalis <miglepage@gmail.com>
2022-04-12 22:49:47 -0500

    Change file to executable file

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,25 +3,19 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
 
-request('https://swapi-api.hbtn.io/api/films/', (error, response, body) => {
+request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    const listFilms = JSON.parse(body).results;
-    for (const film of listFilms) {
-      if (film.url.include(process.argv[2])) {
-        const listPeople = film.characters;
-        for (const people of listPeople) {
-          request(people, (error, response, body) => {
-            if (error) {
-              console.log(error);
-            } else {
-              console.log(JSON.parse(body).name);
-            }
-          });
+    const listPeople = JSON.parse(body).characters;
+    for (const people of listPeople) {
+      request(people, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
         }
-      }
+      });
     }
-    
   }
 });
