b1aa32c6b473e44c8632f2c15df63d3895551b9c
Migdalis <miglepage@gmail.com>
2022-05-23 16:44:44 -0500

    :white_check_mark: Try Fix task 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a GET request to a URL, and displays the body of the response
-curl -s -X GET "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -sLH "X-HolbertonSchool-User-Id: 98" "$1"
