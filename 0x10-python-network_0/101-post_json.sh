b5499ede5915056295cdf47392ba04f52a5646d5
Migdalis <miglepage@gmail.com>
2022-05-23 22:14:52 -0500

    :white_check_mark: Fix task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a JSON POST request and displays the body of the response
-curl -sX POST -d @"$2" "$1"
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
